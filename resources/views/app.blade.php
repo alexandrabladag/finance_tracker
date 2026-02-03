7458a865c9ae6c4bb4aa683cf92c8a5958f53778
Alexandra Ladag <alexandrabladag@gmail.com>
2026-02-03 23:16:31 +0800

    Add Inertia + React setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Finance Tracker</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+</head>
+<body>
+    @inertia
+</body>
+</html>
